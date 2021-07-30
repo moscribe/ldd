0bb8c6077f8f0c91e9358f5229d8cc0d45865f7e
Liu Miao <liumiaogemini@foxmail.com>
2021-07-30 08:53:44 +0000

    add ioctl

--- scull/scull.h
+++ scull/scull.h
@@ -43,5 +43,38 @@ extern int scull_qset;
 int scull_trim(struct scull_dev *dev);
 ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
 ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+long scull_ioctl(struct file* filp, unsigned int cmd, unsigned long arg);
+
+/**
+ * ioctl definitions
+ */
+// magic number
+#define SCULL_IOC_MAGIC 'l'
+
+#define SCULL_IOCRESET _IO(SCULL_IOC_MAGIC, 0)
+
+/**
+ * S means "Set" through a ptr,
+ * T means "Tell" directly with the argument value
+ * G means "Get": reply by setting throuth a pointer
+ * Q means "Query": response is on the return value
+ * X meas "eXchange": switch G and S atomically
+ * H means "sHift":switch T and Q atomically
+ */
+#define SCULL_IOCSQUANTUM	_IOW(SCULL_IOC_MAGIC, 1, int)
+#define SCULL_IOCSQSET		_IOW(SCULL_IOC_MAGIC, 2, int)
+#define SCULL_IOCTQUANTUM	_IO(SCULL_IOC_MAGIC, 3)
+#define SCULL_IOCTQSET		_IO(SCULL_IOC_MAGIC, 4)
+#define SCULL_IOCGQUANTUM	_IOR(SCULL_IOC_MAGIC, 5, int)
+#define SCULL_IOCGQSET		_IOR(SCULL_IOC_MAGIC, 6, int)
+#define SCULL_IOCQQUANTUM	_IO(SCULL_IOC_MAGIC, 7)
+#define SCULL_IOCQQSET		_IO(SCULL_IOC_MAGIC, 8)
+#define SCULL_IOCXQUANTUM	_IOWR(SCULL_IOC_MAGIC, 9, int)
+#define SCULL_IOCXQSET		_IOWR(SCULL_IOC_MAGIC, 10, int)
+#define SCULL_IOCHQUANTUM	_IO(SCULL_IOC_MAGIC, 11)
+#define SCULL_IOCHQSET		_IO(SCULL_IOC_MAGIC, 12)
+
+#define SCULL_IOC_MAXNR 14
+
 
 #endif
